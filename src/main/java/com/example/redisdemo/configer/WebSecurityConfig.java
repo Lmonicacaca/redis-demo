401557ce8545c1a32da8edb71c12f5414bf4a6b4
lilin <lilin8921@adpanshi.com>
2019-01-15 14:13:48 +0800

    security授权

--- src/main/java/com/example/redisdemo/configer/WebSecurityConfig.java
+++ src/main/java/com/example/redisdemo/configer/WebSecurityConfig.java
@@ -1,37 +1,14 @@
 package com.example.redisdemo.configer;
 
-import com.example.redisdemo.component.AccessDecisionManagerImpl;
-import com.example.redisdemo.component.FilterInvocationSecurityMetadataSourceImpl;
+import com.example.redisdemo.component.MyAccessDeniedHandler;
 import com.example.redisdemo.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 @Configuration
 @EnableWebSecurity
@@ -39,9 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
     @Autowired
-    private FilterInvocationSecurityMetadataSourceImpl filterInvocationSecurityMetadataSource;
-    @Autowired
-    private AccessDecisionManager accessDecisionManager;
+    private MyAccessDeniedHandler myAccessDeniedHandler;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -61,7 +36,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable()
-                .exceptionHandling();
+                .exceptionHandling()
+                .accessDeniedHandler(myAccessDeniedHandler);
     }
 
     @Override
--- src/main/java/com/example/redisdemo/controller/MenuController.java
+++ src/main/java/com/example/redisdemo/controller/MenuController.java
@@ -1,16 +1,10 @@
 package com.example.redisdemo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MenuController {
-    @Autowired
-    private FilterInvocationSecurityMetadataSource filterInvocationSecurityMetadataSource;
-
-
 
     @RequestMapping("/toMenu")
     public String toMenu(){
